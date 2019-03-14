912cfc82021a70830086f80797c5d5f4f6d94603
Lucas Rosa Barbosa <lucas.rosa2006@gmail.com>
2019-03-14 10:23:09 -0300

    2019-03-14

---
+++ lista3-javascript/js/questao01.js
@@ -0,0 +1,15 @@
+function bgVermelho(){
+    document.getElementById('paginaInicial').style.backgroundColor = 'red';
+}
+
+function bgVerde(){
+    document.getElementById('paginaInicial').style.backgroundColor = 'green';
+}
+
+function bgAzul(){
+    document.getElementById('paginaInicial').style.backgroundColor = 'blue';
+}
+
+function bgAmarelo(){
+    document.getElementById('paginaInicial').style.backgroundColor = 'yellow';
+}
