d5331d53e63e043b4e03b53ce3f04434f8d42254
Lucas Rosa Barbosa <lucas.rosa2006@gmail.com>
2019-03-14 10:38:10 -0300

    2019-03-14

---
+++ lista3-javascript/js/questao02.js
@@ -0,0 +1,15 @@
+function bgPreto(){
+    document.body.style.backgroundColor = 'black'
+}
+
+function bgCinza(){
+    document.body.style.backgroundColor = 'grey'
+}
+
+function bgLaranja(){
+    document.body.style.backgroundColor = 'orange'
+}
+
+function bgRoxo (){
+    document.body.style.backgroundColor = 'purple'
+}
